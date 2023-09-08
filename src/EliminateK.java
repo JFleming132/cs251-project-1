1b959bbd47736f3b6050b35155579f4a70be76db
JFleming132 <josephflemingvi@gmail.com>
2023-09-08 17:40:29 -0400

    Wrote EliminateK and SumTwoNumbers

--- src/EliminateK.java
+++ src/EliminateK.java
@@ -3,7 +3,38 @@ public class EliminateK {
 
     public int computeSurvivor(int n,int k)
     {
-       return 0;
+       if (n > 0) {
+           int i;
+           IntListNode head = new IntListNode(1);
+           IntListNode current = head;
+           for (i = 2; i <= n; i++) {
+               current.next = new IntListNode(i);
+               current = current.next;
+           }
+           current.next = head;
+           i = 1;
+           int prev_val = 0;
+           while (prev_val != current.val) {
+               if (i % k == 0) { //every kth element
+                   current.next = current.next.next; //remove next in list (iteration starts at tail and loops)
+               }
+               i++; //increment index
+               prev_val = current.val; //bookmark previous value
+               current = current.next; //advance list worker
+           }
+           return prev_val;
+       }
+       else {
+           return 0;
+       }
     }
+}
 
+class IntListNode {
+    int val;
+    IntListNode next;
+
+    public IntListNode(int v) {
+        val = v;
+    }
 }
--- src/SumTwoNumbers.java
+++ src/SumTwoNumbers.java
@@ -11,6 +11,83 @@ public class SumTwoNumbers {
     public Node num2_list;
 
     public Node add(String num1, String num2) {
-        return new Node();
+        Node adder1;
+        Node adder2;
+        Node returnList;
+        Node head = null;
+        int currentDigit;
+        int numIndex;
+        for(numIndex = 0; numIndex < num1.length(); numIndex++) {
+            currentDigit = Character.getNumericValue(num1.charAt(numIndex));
+            if (numIndex == 0) {
+                num1_list = new Node();
+                head = num1_list;
+                head.key = currentDigit;
+                head.next = null;
+                head.previous = null;
+            }
+            else {
+                head.next = new Node();
+                head.next.previous = head;
+                head = head.next;
+                head.key = currentDigit;
+                head.next = null;
+            }
+        }
+        for(numIndex = 0; numIndex < num2.length(); numIndex++) {
+            currentDigit = Character.getNumericValue(num2.charAt(numIndex));
+            if (numIndex == 0) {
+                num2_list = new Node();
+                head = num2_list;
+                head.key = currentDigit;
+                head.next = null;
+                head.previous = null;
+            }
+            else {
+                head.next = new Node();
+                head.next.previous = head;
+                head = head.next;
+                head.key = currentDigit;
+                head.next = null;
+            }
+        }
+        adder1 = num1_list;
+        adder2 = num2_list;
+        while(adder1.next != null) {
+            adder1 = adder1.next;
+        }
+        while(adder2.next != null) {
+            adder2 = adder2.next;
+        }
+        returnList = new Node();
+        returnList.key = 0;
+        int adder1Digit, adder2Digit;
+        currentDigit = 0;
+        while(true) {
+            if (adder1 != null) {
+                adder1Digit = adder1.key;
+                adder1 = adder1.previous;
+            }
+            else {
+                adder1Digit = 0;
+            }
+            if (adder2 != null) {
+                adder2Digit = adder2.key;
+                adder2 = adder2.previous;
+            }
+            else {
+                adder2Digit = 0;
+            }
+            currentDigit = adder1Digit + adder2Digit + currentDigit;
+            returnList.key = currentDigit % 10;
+            currentDigit = currentDigit / 10;
+            if ((currentDigit == 0) && (adder1 == null) && (adder2 == null)) {
+                break;
+            }
+            returnList.previous = new Node();
+            returnList.previous.next = returnList;
+            returnList = returnList.previous;
+        }
+        return returnList;
     }
 }
